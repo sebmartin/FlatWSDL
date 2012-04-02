d73d5d5b69c45e90da3ef93b17fe41ae72561c88
Andreas Maechler <andreas.maechler@varian.com>
2012-04-02 16:23:04 +0200

    Prevent URL from getting included twice and add a verbose mode

--- flatten-wsdl.py
+++ flatten-wsdl.py
@@ -5,6 +5,8 @@ import urllib
 from optparse import OptionParser
 from subprocess import call
 
+# List of URL to skip
+url_skiplist = []
 
 def importUrl(url, parent, after):
 	insertOffset = 1
@@ -13,23 +15,26 @@ def importUrl(url, parent, after):
 		parent.insert(parent.getchildren().index(after) + insertOffset, child)
 		insertOffset += 1
 
-def flattenImports(parent, tag, skip):
-	
+def flattenImports(parent, tag):
  	while True:
 		importEl = parent.find(tag)
 		if importEl == None:
 			break
-			
+		
+		# Get URL from location or schemaLocation tag
 		url = importEl.get('location')
 		if not url:
 			url = importEl.get('schemaLocation')
 		if not url:
 			return
-		if not url in skip:
-			importUrl(url, parent, importEl)
-			skip.append(url)
-		parent.remove(importEl)
 
+		# Import the URLs found and replace their content with the import line
+		if not url in url_skiplist:
+			if options.verbose:
+				print('Importing URL: ' + url)
+			importUrl(url, parent, importEl)
+			url_skiplist.append(url)
+		parent.remove(importEl)
 
 def loadTreeFromUrl(url):
 	fp = urllib.urlopen(url)
@@ -42,11 +47,11 @@ def flattenWsdl(url, output):
 	root = tree.getroot()
 	
 	# Process WSDL imports
-	flattenImports(root, '{http://schemas.xmlsoap.org/wsdl/}import', [])
+	flattenImports(root, '{http://schemas.xmlsoap.org/wsdl/}import')
 	
 	# Process XSD imports
 	for schema in root.findall('.//{http://www.w3.org/2001/XMLSchema}schema'):
-		flattenImports(schema, '{http://www.w3.org/2001/XMLSchema}import', [])
+		flattenImports(schema, '{http://www.w3.org/2001/XMLSchema}import')
 	
 	tree.write(output)
 	
@@ -59,6 +64,7 @@ parser.add_option('-n', '--namespace', action="store", type="string", dest="name
 	help='Override the default namespace')
 parser.add_option('-t', '--tidy', action="store_true",
 	help='Run |tidy| on the file after flattening. Requires -f')
+parser.add_option('-v', '--verbose', action="store_true", help='Enable verbose output')
 (options, args) = parser.parse_args()
 
 if options.namespace:
@@ -74,8 +80,11 @@ out = open(options.filename, 'w+') if options.filename else sys.stdout
 try:
 	flattenWsdl(args[0], out)
 finally:
-	print "\nFlattening done."
+	print "Flattening done."
 	out.close()
 
 if options.tidy:
-	call(['tidy', '-q', '-mi', '-xml', options.filename])
+	tidy_args = ['tidy', '-mi', '-xml', options.filename]
+	if not options.verbose:
+		tidy_args.insert(1, '-q')
+	call(tidy_args)
